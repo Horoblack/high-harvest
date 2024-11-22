ecf2e9476ba67d556c4c5be850b7594ab25bd71a
Acherow <victorhugodias1@hotmail.com>
2024-11-22 01:34:50 -0300

    gsfxvbj

--- scripts/scarecrow.gd
+++ scripts/scarecrow.gd
@@ -7,10 +7,12 @@ var anchored : bool = false
 
 func _physics_process(delta):
 	if(cast.is_colliding() && !anchored):
-		joint.node_b = cast.get_collider().get_path()
-		anchored = true
-		joint.enabled = true
-		gravity_scale = 0
+		var normal : Vector3 = cast.get_collision_normal()
+		if(normal.dot(global_basis.y) > .7):
+			joint.node_b = cast.get_collider().get_path()
+			anchored = true
+			joint.enabled = true
+			gravity_scale = 0
 	if(!cast.is_colliding() && anchored):
 		joint.node_b = NodePath("")
 		joint.enabled = false
