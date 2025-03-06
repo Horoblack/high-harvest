db1099d55134cb7b95901c680643e929999005f0
Acherow <victorhugodias1@hotmail.com>
2025-03-06 15:58:21 -0300

    jrk

--- scripts/tap.gd
+++ scripts/tap.gd
@@ -2,6 +2,14 @@ extends StaticBody3D
 
 var curobj
 
+var on:bool = false
+
+@onready var wat: MeshInstance3D = $MeshInstance3D
+
+func grabtrigger(pl : Player):
+	on = !on
+	wat.visible = on
+
 func _on_area_3d_body_entered(body):
 	if(body.has_method("water")):
 		curobj = body
@@ -12,7 +20,7 @@ func _on_area_3d_body_exited(body):
 		curobj = null
 
 func _on_timer_timeout():
-	if(curobj && curobj.has_method("water")):
+	if(curobj && curobj.has_method("water") && on):
 		curobj.water()
-	else:
+	elif !on:
 		$Timer.stop()
